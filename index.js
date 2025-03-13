c5018519d35587be66f6a5d8ff2fd7729fd61a6f
Joseph <j.mburu.pro@gmail.com>
2025-03-13 18:03:40 +0300

    fixed bug

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 // Write your code here!
-const element = document.createElement("div");
-document.body.append(element);
-
-const ul = document.createElement("ul");
-for (let i = 0; i < 3; i++) {
-    const li = document.createElement("li");
-    li.textContent = (i + 1).toString();
-    ul.append(li);
-}
-element.append(ul);
-
 // Remove the <main> element with id "main"
 const mainElement = document.querySelector("main#main");
 if (mainElement) {
